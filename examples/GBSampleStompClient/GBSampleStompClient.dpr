f46097b6945d8e437d3379cc7e356df965b4ae4b
gabrielbaltazar.f1@gmail.com <gabrielbaltazar>
2020-12-01 18:46:31 -0300

    GBStomp Interface

---
+++ examples/GBSampleStompClient/GBSampleStompClient.dpr
@@ -0,0 +1,14 @@
+program GBSampleStompClient;
+
+uses
+  Vcl.Forms,
+  FMain in 'FMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
